1dd998f5d6233cfeaddf18cfb7ab3a4e766a162a
Carl Rosengren <carl.rosengren1@hotmail.com>
2023-09-21 20:37:50 +0200

    Changed configured context-path to /api/v1/

--- src/main/java/com/carlros/secureapi/config/SecurityConfig.java
+++ src/main/java/com/carlros/secureapi/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers( "/api/v1/auth/**")
+                        authorize.requestMatchers( "/auth/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated());
