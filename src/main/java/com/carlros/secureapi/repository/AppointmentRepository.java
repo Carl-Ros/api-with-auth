afbb6c7c570a8ffcbf3b1e77a464f2b790a50c60
Carl Rosengren <carl.rosengren1@hotmail.com>
2023-10-04 20:59:57 +0200

    nested appointment endpoints under /workspaces/{id}

--- src/main/java/com/carlros/secureapi/repository/AppointmentRepository.java
+++ src/main/java/com/carlros/secureapi/repository/AppointmentRepository.java
@@ -3,5 +3,10 @@ package com.carlros.secureapi.repository;
 import com.carlros.secureapi.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findAllByWorkspaceId(Long workspaceId);
+    Optional<Appointment> findByIdAndWorkspaceId(Long workspaceId, Long id);
 }
